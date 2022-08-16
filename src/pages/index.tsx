397adb8f6ea75f409234f1e11749ce389ba37cf0
Diego Nunes <diegonunesdev@gmail.com>
2022-08-16 03:15:32 -0300

    finished chapter III

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,7 +7,7 @@ export default function SignIn() {
       <Flex as='form' w='100%' maxWidth='360' bg='gray.800' p='8' borderRadius='8' flexDir='column'>
         <Stack spacing='4'>
           <Input type='email' name='email' label='E-mail' />
-          <Input type='password' name='password' label='Password' />
+          <Input type='password' name='password' label='Senha' />
         </Stack>
         <Button type='submit' mt='6' colorScheme='pink' size='lg'>
           Entrar
